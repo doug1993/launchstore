b114a8a900476cd260c1751b57528587621c4a8f
doug1993 <62299777+doug1993@users.noreply.github.com>
2020-06-17 22:12:34 -0300

    Delete db.js

--- src/config/db.js
+++
@@ -1,9 +0,0 @@
-const {Pool} = require('pg')
-module.exports= new Pool( {
-    user  :'dougfortes',
-    password:'@AUWkhf1419',
-    host:'localhost',
-    port:5432,
-    database:'launchstoredb'
-
-})
